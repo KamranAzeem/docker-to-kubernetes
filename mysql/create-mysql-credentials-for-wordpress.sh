d69d7dbf731b3e20a03e61b9093b5946be4c3b8a
Kamran Azeem <kaz@praqma.net>
2020-04-16 01:01:37 +0200

    added some support files to ease  mysql deployment

---
+++ mysql/create-mysql-credentials-for-wordpress.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# This script reads mysql-wordpress.env and creates equivalent kubernetes secrets.
+# It needs to be capable for creating k8s secrets by reading ENV variables as well,
+#   as, that is the case with CI systems.
+if [ ! -f ./mysql-wordpress.env ]; then
+  echo "Could not find ENV variables file for mysql - ./mysql-wordpress.env"
+  exit 1
+fi
+
+echo "First delete the old secret: mysql-credentials"
+kubectl delete secret mysql-credentials  || true
+
+echo "Found mysql-wordpress.env file, creating kubernetes secret: mysql-credentials"
+source ./mysql-wordpress.env
+
+
+kubectl create secret generic mysql-credentials \
+  --from-literal=MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD} \
+  --from-literal=MYSQL_DATABASE=${MYSQL_DATABASE} \
+  --from-literal=MYSQL_USER=${MYSQL_USER} \
+  --from-literal=MYSQL_PASSWORD=${MYSQL_PASSWORD}
+
+
